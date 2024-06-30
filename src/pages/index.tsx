978ae624b598f41cda70f1c657ee36f15382ea31
Junshen18 <108778342+Junshen18@users.noreply.github.com>
2024-06-30 15:24:15 +0800

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,16 @@ import Progress from "../components/progress";
 import Footer from "@/components/layout/Footer";
 
 export default function Home() {
-	return (
-		<main className={`flex min-h-screen flex-col items-center justify-between px-[70px]`}>
-			<WhyKreate />
-			<div className='py-[200px] text-center text-2xl '>What we&lsquo;re kreating</div>
-			<Progress />
-			<Footer />
-		</main>
-	);
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between px-[70px]`}
+    >
+      <WhyKreate />
+      <div className="py-[200px] text-center text-2xl ">
+        What we&lsquo;re 'kreate'ing
+      </div>
+      <Progress />
+      <Footer />
+    </main>
+  );
 }
