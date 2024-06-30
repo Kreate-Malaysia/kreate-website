1ea5c33c16fffcae19999ec94803f2430a7a515f
AaronTan11 <tanweihup@gmail.com>
2024-06-30 10:56:58 +0800

    fix: changed routes to components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,15 @@
 import Image from "next/image";
-import WhyKreate from "./whykreate";
-import Progress from "./progress";
+import WhyKreate from "../components/whykreate";
+import Progress from "../components/progress";
 import Footer from "@/components/layout/Footer";
 
-
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between px-[70px]`}
-    >
-      <WhyKreate/>
-      <div className="py-[200px] text-center text-2xl ">
-        What we&lsquo;re kreating
-      </div>
-      <Progress/>
-      <Footer/>
-    </main>
-  );
+	return (
+		<main className={`flex min-h-screen flex-col items-center justify-between px-[70px]`}>
+			<WhyKreate />
+			<div className='py-[200px] text-center text-2xl '>What we&lsquo;re kreating</div>
+			<Progress />
+			<Footer />
+		</main>
+	);
 }
