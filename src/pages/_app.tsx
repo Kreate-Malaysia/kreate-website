28f55c56745b5e227b80da026cb78139864250c6
AaronTan11 <tanweihup@gmail.com>
2024-07-01 22:46:53 +0800

    trying

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,6 @@ import type { AppProps } from "next/app";
 import { Manrope } from "next/font/google";
 const manrope = Manrope({ subsets: ["latin"] });
 
-export const runtime = "edge";
-
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<main className={manrope.className}>
