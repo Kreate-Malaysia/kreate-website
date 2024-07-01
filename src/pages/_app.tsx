04a20fe4a78e3e3fba95df75544fd7a75a944ed0
Johnson Chin <johnsonchin1009@gmail.com>
2024-07-01 21:42:55 +0800

    feat: added Navbar for both desktop and mobile

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import Navbar from "@/components/Navbar";
 import type { AppProps } from "next/app";
 import { Manrope } from "next/font/google";
 const manrope = Manrope({subsets: ['latin']})
@@ -6,6 +7,7 @@ const manrope = Manrope({subsets: ['latin']})
 export default function App({ Component, pageProps }: AppProps) {
   return (
   <main className={manrope.className}>
+      <Navbar />
       <Component {...pageProps} />;
   </main>
 
